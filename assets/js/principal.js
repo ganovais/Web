0a81be917674569792214453a1785872cf36b9ce
ganovais <gabriel_novais1997@hotmail.com>
2020-10-23 21:58:32 -0300

    Aula10 de js

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -3,6 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('.add-item-cart').forEach(button => {
         button.onclick = () => {
             document.querySelector('.toaster').classList.remove('d-none');
+
+            let src = button.parentElement.parentElement.children[0].children[1].getAttribute('src');
+
+            document.querySelector('#toaster-img').setAttribute('src', src);
+
+            let name = button.parentElement.parentElement.children[1].children[0].innerHTML;
+
+            let price = button.parentElement.parentElement.children[1].children[1].children[0].innerHTML;
+
+            document.querySelector('.toaster-content').innerHTML = `
+                <h4>${name}</h4>
+                <p><b>${price}</b></p>
+            `;
         }
     })
 
@@ -13,4 +26,8 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 3000);
 
     })
+
+    document.querySelector('.fa-times').onclick = () => {
+        document.querySelector('.toaster').classList.add('d-none');
+    }
 })
